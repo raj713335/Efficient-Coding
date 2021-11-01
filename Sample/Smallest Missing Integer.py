191a84b7aaaa92cf313ebae0e111f041badedb00
ARNAB DAS <raj713335@gmail.com>
2021-11-01 18:34:40 +0530

    Add files via upload

---
+++ Sample/Smallest Missing Integer.py
@@ -0,0 +1,28 @@
+def smallMissingNumber(a, n) :  
+      
+    #unordered map 
+    mp = dict();  
+  
+    #checking value of array is positive or not
+    #if positive, store in mp 
+    for i in range(n) : 
+        if (a[i] > 0) : 
+            if a[i] not in mp.keys() : 
+                mp[a[i]] = 0
+            
+            mp[a[i]] += 1
+  
+    index = 1;  
+ 
+    while (1) : 
+        if (index not in mp.keys()) :  
+            return index;  
+          
+  
+        index += 1;  
+
+  
+arr = [ -5, 2, 0, -1, -10, 15 ];  
+size = len(arr);  
+  
+print("Smallest positive missing number is : ",smallMissingNumber(arr, size));  
