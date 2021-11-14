150f57312f1416126ef25377c3e4cb239dc6b596
ARNAB DAS <raj713335@gmail.com>
2021-11-15 02:38:09 +0530

    Update 08 Convert String.py

--- Assignment/08 Convert String.py
+++ Assignment/08 Convert String.py
@@ -1,3 +1,4 @@
+"""
 for k in range(0, int(input())):
     X = list((input()))
     Y = list((input()))
@@ -17,9 +18,79 @@ for k in range(0, int(input())):
     else:
         print("NO")
 
+"""
 
+def solve(X, Y):
 
+  N, M = len(X), len(Y)
 
+  DP = [[ None for _ in range(M+1)] for _ in range(N+1)]
+
+   
+
+  # Initialization
+
+  for i in range(M+1):
+
+    DP[0][i] = False
+
+  DP[0][0] = True
+
+  for i in range(1, N+1):
+
+    if X[i-1].islower():
+
+      DP[i][0] = DP[i-1][0]
+
+    else:
+
+      DP[i][0] = False
+
+   
+
+   
+
+  for i in range(1, N+1):
+
+    for j in range(1, M+1):
+
+      if X[i-1].isupper() and X[i-1] == Y[j-1]:
+
+        DP[i][j] = DP[i-1][j-1]
+
+      elif X[i-1].isupper():
+
+        DP[i][j] = False 
+
+      elif X[i-1].islower() and ((X[i-1] == Y[j-1]) or (X[i-1].upper() == Y[j-1])):
+
+        DP[i][j] = DP[i-1][j-1] | DP[i-1][j]
+
+      elif X[i-1].islower():
+
+        DP[i][j] = DP[i-1][j]
+
+   
+
+  return DP[N][M]
+
+   
+
+if __name__ == "__main__":
+
+  Q = int(input())
+
+  for _ in range(Q):
+
+    X,Y = input(), input()
+
+    if solve(X,Y):
+
+      print("YES")
+
+    else:
+
+      print("NO")
 
 """
 2
